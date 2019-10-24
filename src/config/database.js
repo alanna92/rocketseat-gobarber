f874f98a496a6f19491ecc0adc8c80d9bcaa21fa
Alanna Vanzella <alanna92@gmail.com>
2019-10-24 19:01:40 -0300

    First commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: '',
+  password: '',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
