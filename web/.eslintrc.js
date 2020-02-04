7d9593ab4a06c7fe647c52ee0a74043a97317ddd
Dario Lacerda <dariomateuswl@gmail.com>
2020-02-04 20:08:43 -0300

    Add  private routes and layouts

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -37,6 +37,14 @@ module.exports = {
     'no-console': ['error', {allow: ['tron']}],
 
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        'rootPathSuffix': 'src'
+      },
+    },
   },
 };
--- web/src/App.js
+++ web/src/App.js
@@ -4,12 +4,15 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
-import ReactotronConfig from './config/ReactotronConfig';
+import './config/ReactotronConfig';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
